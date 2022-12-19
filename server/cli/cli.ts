cac1a548dfd8d59a0fcd884744663885d04adac1
thqby <zhenghao_707@126.com>
2022-12-19 20:55:03 +0800

    Added cli.js

---
+++ server/cli/cli.ts
@@ -0,0 +1,25 @@
+import { openFile } from '../src/common';
+import { Lexer } from '../src/Lexer';
+
+function main() {
+	let options: any = {};
+	process.argv.slice(2).map(s => {
+		let arr = s.split('=');
+		options[arr[0]] = arr[1];
+	});
+	let path: string = options.path ?? '';
+	path = path.replace(/^(['"])(.*)\1$/, '$2');
+	if (!path)
+		return;
+	try {
+		let td = openFile(path, false);
+		if (!td)
+			return;
+		let sc = new Lexer(td).beautify();
+		console.log(sc);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+main()
