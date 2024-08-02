80d9c417963d418522c3adfe669390c486b490d2
Mark Wiemer <markwiemer@outlook.com>
2024-08-01 22:29:14 -0700

    Add sample test

--- client/.vscode-test.mjs
+++ client/.vscode-test.mjs
@@ -1,3 +1,6 @@
 // https://github.com/microsoft/vscode-test-cli
 import { defineConfig } from '@vscode/test-cli';
-export default defineConfig({ files: 'out/test/**/*.test.js' });
+export default defineConfig({
+	files: 'dist/test/**/*.test.js',
+	version: '1.92.0'
+});
