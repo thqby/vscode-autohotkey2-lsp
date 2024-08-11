db4b7182e061935a98ecc6b4a43c8b1fdbb23b94
Mark Wiemer <markwiemer@outlook.com>
2024-08-10 20:54:40 -0700

    Indent with tabs

--- client/src/test/extension.test.ts
+++ client/src/test/extension.test.ts
@@ -4,25 +4,25 @@ import * as fs from 'fs';
 import { resolvePath } from '../extension';
 
 suite('resolvePath', () => {
-    let lstatSyncStub: sinon.SinonStub;
+	let lstatSyncStub: sinon.SinonStub;
 
-    suiteSetup(() => {
-        lstatSyncStub = sinon.stub(fs, 'lstatSync');
-    });
+	suiteSetup(() => {
+		lstatSyncStub = sinon.stub(fs, 'lstatSync');
+	});
 
-    const theories: ([string, string] | [string, string, boolean])[] = [
-        ['', ''],
-        ['C:/out.txt', 'C:/out.txt'],
-    ];
+	const theories: ([string, string] | [string, string, boolean])[] = [
+		['', ''],
+		['C:/out.txt', 'C:/out.txt'],
+	];
 
-    theories.forEach(([input, expected, isSymbolicLink = false]) => {
-        test(`resolvePath('${input}') === '${expected}'`, () => {
-            // Mock the behavior of fs.lstatSync
-            lstatSyncStub
-                .withArgs(input)
-                .returns({ isSymbolicLink: () => isSymbolicLink });
+	theories.forEach(([input, expected, isSymbolicLink = false]) => {
+		test(`resolvePath('${input}') === '${expected}'`, () => {
+			// Mock the behavior of fs.lstatSync
+			lstatSyncStub
+				.withArgs(input)
+				.returns({ isSymbolicLink: () => isSymbolicLink });
 
-            assert.strictEqual(resolvePath(input), expected);
-        });
-    });
+			assert.strictEqual(resolvePath(input), expected);
+		});
+	});
 });
