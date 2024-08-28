289244990f1a6c836417936dd7cf140c68089847
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-08-27 18:57:27 -0700

    Build with ESBuild (#13)

---
+++ build.mjs
@@ -0,0 +1,30 @@
+import { build } from 'esbuild';
+import path from 'path';
+
+const isProd = process.argv.indexOf('--mode=production') >= 0;
+
+// Node server
+// https://esbuild.github.io/api
+build({
+	entryPoints: [path.join('./server/src/server.ts')],
+	bundle: true,
+	outfile: path.join('./server/dist/server.js'),
+	external: ['vscode'],
+	format: 'cjs',
+	platform: 'node',
+	minify: isProd,
+	sourcemap: !isProd,
+});
+
+// Node client (not necessary for AHK++, but super fast)
+// https://esbuild.github.io/api
+build({
+	entryPoints: [path.join('./client/src/extension.ts')],
+	bundle: true,
+	outfile: path.join('./client/dist/extension.js'),
+	external: ['vscode'],
+	format: 'cjs',
+	platform: 'node',
+	minify: isProd,
+	sourcemap: !isProd,
+});
