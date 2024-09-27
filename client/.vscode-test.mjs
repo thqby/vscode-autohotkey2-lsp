aae4f9e45fda8447df3f8a52143aaa5286425cce
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-09-26 20:54:42 -0700

    Remove dupe config file (#31)

--- client/.vscode-test.mjs
+++
@@ -1,7 +0,0 @@
-// https://github.com/microsoft/vscode-test-cli
-import { defineConfig } from '@vscode/test-cli';
-export default defineConfig({
-	extensionDevelopmentPath: '..',
-	files: 'dist/**/*.test.js',
-	version: '1.92.0',
-});
