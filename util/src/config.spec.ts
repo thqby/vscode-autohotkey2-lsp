fb659f50430c5ef02bbbe3d09f796e0d65afe082
Mark Wiemer <markwiemer@outlook.com>
2024-10-05 07:28:02 -0700

    Fixup tests for setCfg

--- util/src/config.spec.ts
+++ util/src/config.spec.ts
@@ -3,17 +3,15 @@ import { suite, test } from 'mocha';
 import {
 	AHKLSConfig,
 	CfgKey,
-	FormatOptions,
 	getCfg,
 	LibIncludeType,
+	newConfig,
 	setCfg,
 } from './config';
 
 suite('setCfg', () => {
 	test('top-level property', () => {
-		const cfg: AHKLSConfig = {
-			AutoLibInclude: LibIncludeType.All,
-		} as AHKLSConfig;
+		const cfg: AHKLSConfig = newConfig();
 
 		setCfg(CfgKey.LibrarySuggestions, LibIncludeType.Disabled, cfg);
 
@@ -21,18 +19,45 @@ suite('setCfg', () => {
 			LibIncludeType.Disabled,
 			getCfg(CfgKey.LibrarySuggestions, cfg),
 		);
+
+		setCfg(CfgKey.LibrarySuggestions, LibIncludeType.All, cfg);
+
+		assert.strictEqual(
+			LibIncludeType.All,
+			getCfg(CfgKey.LibrarySuggestions, cfg),
+		);
 	});
 
 	test('nested property', () => {
-		const cfg: AHKLSConfig = {
-			FormatOptions: { brace_style: 1 },
-		} as AHKLSConfig;
+		const cfg: AHKLSConfig = newConfig();
 
-		setCfg(CfgKey.Formatter, { brace_style: 2 }, cfg);
+		setCfg(CfgKey.CallWithoutParentheses, false, cfg);
 
 		assert.strictEqual(
-			2,
-			getCfg<FormatOptions>(CfgKey.Formatter, cfg).brace_style,
+			false,
+			getCfg<boolean>(CfgKey.CallWithoutParentheses, cfg),
 		);
+
+		setCfg(CfgKey.CallWithoutParentheses, true, cfg);
+
+		assert.strictEqual(
+			true,
+			getCfg<boolean>(CfgKey.CallWithoutParentheses, cfg),
+		);
+	});
+
+	test('nested object property', () => {
+		const cfg: AHKLSConfig = newConfig();
+
+		setCfg<number>(CfgKey.ArrayStyle, 1, cfg);
+		setCfg<number>(CfgKey.BraceStyle, 0, cfg);
+
+		assert.strictEqual(0, getCfg<boolean>(CfgKey.BraceStyle, cfg));
+
+		setCfg(CfgKey.ArrayStyle, 2, cfg);
+
+		// Check for updated value and other value not deleted
+		assert.strictEqual(2, getCfg<boolean>(CfgKey.ArrayStyle, cfg));
+		assert.strictEqual(0, getCfg<boolean>(CfgKey.BraceStyle, cfg));
 	});
 });
