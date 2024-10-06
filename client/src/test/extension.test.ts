99fe868d6afcf0658224b55884636ff2f9812a0b
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-10-06 09:09:17 -0700

    Cleanup tests, clean before building (#49)

--- client/src/test/extension.test.ts
+++ client/src/test/extension.test.ts
@@ -42,197 +42,200 @@ import {
 	DidChangeConfigurationNotification,
 } from 'vscode-languageclient/node';
 import { readdirSync } from 'fs';
+import { suite, before, test } from 'mocha';
 
-suite('Start ahk language server', () => {
-	test('should be running', async () => {
-		await vscode.commands.executeCommand('workbench.action.closeAllEditors');
-		const client: LanguageClient = await vscode.extensions
-			.getExtension('thqby.vscode-autohotkey2-lsp')
-			?.activate();
-		assert.equal(client?.isRunning(), true);
+let client: LanguageClient;
+before(async () => {
+	client = await vscode.extensions
+		.getExtension('thqby.vscode-autohotkey2-lsp')
+		?.activate();
+	await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+});
 
-		suite('Open ahk file', () => {
-			test('should be opened', async () => {
-				const path = resolve(__dirname, '../../../server/dist/ahkProvider.ahk');
-				let document = await vscode.workspace.openTextDocument(path);
-				const uri = document.uri.toString();
-				await vscode.window.showTextDocument(document);
-				if (document.languageId !== 'ahk2')
-					document = await vscode.languages.setTextDocumentLanguage(
-						document,
-						'ahk2',
-					);
-				const content = (await client.sendRequest(
-					'ahk2.getContent',
-					uri,
-				)) as string;
-				assert.equal(document.getText() === content, true);
+test('should be running', async () => {
+	assert.equal(client?.isRunning(), true);
+});
 
-				suite('Test language server features', () => {
-					const textDocument = { uri };
-					const position = { line: 10, character: 5 };
+suite('Open AHK file', () => {
+	test('opens', async () => {
+		const path = resolve(__dirname, '../../../server/dist/ahkProvider.ahk');
+		let document = await vscode.workspace.openTextDocument(path);
+		const uri = document.uri.toString();
+		await vscode.window.showTextDocument(document);
+		if (document.languageId !== 'ahk2')
+			document = await vscode.languages.setTextDocumentLanguage(
+				document,
+				'ahk2',
+			);
+		const content = (await client.sendRequest(
+			'ahk2.getContent',
+			uri,
+		)) as string;
+		assert.equal(document.getText() === content, true);
 
-					test(CompletionRequest.method, async function () {
-						const params: CompletionParams = { textDocument, position };
-						const result: CompletionItem[] | undefined =
-							await client.sendRequest(this.runnable().title, params);
-						assert.ok(result?.length);
-					});
+		suite('Send language server requests', () => {
+			const textDocument = { uri };
+			const position = { line: 10, character: 5 };
 
-					test(DefinitionRequest.method, async function () {
-						const params: DefinitionParams = { textDocument, position };
-						const result: LocationLink[] | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.length);
-					});
-
-					test(DocumentFormattingRequest.method, async function () {
-						const params: DocumentFormattingParams = {
-							textDocument,
-							options: { insertSpaces: false, tabSize: 4 },
-						};
-						const result: TextEdit[] | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.length);
-					});
-
-					test(DocumentSymbolRequest.method, async function () {
-						const params: DocumentSymbolParams = { textDocument };
-						const result: SymbolInformation[] | undefined =
-							await client.sendRequest(this.runnable().title, params);
-						assert.ok(result?.length);
-					});
-
-					test(FoldingRangeRequest.method, async function () {
-						const params: FoldingRangeParams = { textDocument };
-						const result: FoldingRange[] | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.length);
-					});
-
-					test(HoverRequest.method, async function () {
-						const params: HoverParams = { textDocument, position };
-						const result: Hover | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.contents);
-					});
-
-					test(PrepareRenameRequest.method, async function () {
-						const params: PrepareRenameParams = { textDocument, position };
-						const result: unknown | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result);
-					});
-
-					test(ReferencesRequest.method, async function () {
-						const params: ReferenceParams = {
-							textDocument,
-							position,
-							context: { includeDeclaration: true },
-						};
-						const result: Location[] | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.length);
-					});
-
-					test(RenameRequest.method, async function () {
-						const params: RenameParams = {
-							textDocument,
-							position,
-							newName: '',
-						};
-						const result: WorkspaceEdit | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.changes);
-					});
-
-					test(SignatureHelpRequest.method, async function () {
-						const params: SignatureHelpParams = {
-							textDocument,
-							position: { line: 8, character: 36 },
-						};
-						const result: SignatureHelp | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.signatures);
-					});
-
-					test(ExecuteCommandRequest.method, async function () {
-						const params: ExecuteCommandParams = {
-							command: 'ahk2.diagnose.all',
-						};
-						await client.sendRequest(this.runnable().title, params);
-					});
-
-					test(WorkspaceSymbolRequest.method, async function () {
-						const params: WorkspaceSymbolParams = { query: 'msg' };
-						const result: SymbolInformation[] | undefined =
-							await client.sendRequest(this.runnable().title, params);
-						assert.ok(result?.length);
-					});
-
-					test(SemanticTokensRequest.method, async function () {
-						const params: SemanticTokensParams = { textDocument };
-						const result: SemanticTokens | undefined = await client.sendRequest(
-							this.runnable().title,
-							params,
-						);
-						assert.ok(result?.data);
-					});
-				});
-
-				await client.sendNotification(
-					DidChangeConfigurationNotification.method,
-					{ settings: { FormatOptions: {} } },
+			test(CompletionRequest.method, async function () {
+				const params: CompletionParams = { textDocument, position };
+				const result: CompletionItem[] | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
 				);
-				test_fotmatting(client);
+				assert.ok(result?.length);
+			});
+
+			test(DefinitionRequest.method, async function () {
+				const params: DefinitionParams = { textDocument, position };
+				const result: LocationLink[] | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.length);
+			});
+
+			test(DocumentFormattingRequest.method, async function () {
+				const params: DocumentFormattingParams = {
+					textDocument,
+					options: { insertSpaces: false, tabSize: 4 },
+				};
+				const result: TextEdit[] | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.length);
+			});
+
+			test(DocumentSymbolRequest.method, async function () {
+				const params: DocumentSymbolParams = { textDocument };
+				const result: SymbolInformation[] | undefined =
+					await client.sendRequest(this.runnable().title, params);
+				assert.ok(result?.length);
+			});
+
+			test(FoldingRangeRequest.method, async function () {
+				const params: FoldingRangeParams = { textDocument };
+				const result: FoldingRange[] | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.length);
+			});
+
+			test(HoverRequest.method, async function () {
+				const params: HoverParams = { textDocument, position };
+				const result: Hover | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.contents);
+			});
+
+			test(PrepareRenameRequest.method, async function () {
+				const params: PrepareRenameParams = { textDocument, position };
+				const result: unknown | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result);
+			});
+
+			test(ReferencesRequest.method, async function () {
+				const params: ReferenceParams = {
+					textDocument,
+					position,
+					context: { includeDeclaration: true },
+				};
+				const result: Location[] | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.length);
+			});
+
+			test(RenameRequest.method, async function () {
+				const params: RenameParams = {
+					textDocument,
+					position,
+					newName: '',
+				};
+				const result: WorkspaceEdit | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.changes);
+			});
+
+			test(SignatureHelpRequest.method, async function () {
+				const params: SignatureHelpParams = {
+					textDocument,
+					position: { line: 8, character: 36 },
+				};
+				const result: SignatureHelp | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.signatures);
+			});
+
+			test(ExecuteCommandRequest.method, async function () {
+				const params: ExecuteCommandParams = {
+					command: 'ahk2.diagnose.all',
+				};
+				await client.sendRequest(this.runnable().title, params);
+			});
+
+			test(WorkspaceSymbolRequest.method, async function () {
+				const params: WorkspaceSymbolParams = { query: 'msg' };
+				const result: SymbolInformation[] | undefined =
+					await client.sendRequest(this.runnable().title, params);
+				assert.ok(result?.length);
+			});
+
+			test(SemanticTokensRequest.method, async function () {
+				const params: SemanticTokensParams = { textDocument };
+				const result: SemanticTokens | undefined = await client.sendRequest(
+					this.runnable().title,
+					params,
+				);
+				assert.ok(result?.data);
 			});
 		});
 	});
 });
 
-function test_fotmatting(client: LanguageClient) {
-	suite('Test formatting', () => {
-		const dir = resolve(__dirname, '../../src/test/formatting');
-		const files = readdirSync(dir);
-		for (const file of files) {
-			if (!file.endsWith('.ahk')) continue;
-			test(file.slice(0, -4), async function () {
-				let document = await vscode.workspace.openTextDocument(
-					resolve(dir, file),
-				);
-				if (document.languageId !== 'ahk2')
-					document = await vscode.languages.setTextDocumentLanguage(
-						document,
-						'ahk2',
-					);
-				const uri = document.uri.toString();
-				const params: DocumentFormattingParams = {
-					textDocument: { uri },
-					options: { insertSpaces: false, tabSize: 4 },
-				};
-				const content = document.getText().replaceAll('\r\n', '\n');
-				const result: TextEdit[] | undefined = await client.sendRequest(
-					DocumentFormattingRequest.method,
-					params,
-				);
-				assert.ok(result?.[0].newText === content);
-			});
-		}
+suite('Formatting', async () => {
+	before(async () => {
+		await client.sendNotification(DidChangeConfigurationNotification.method, {
+			settings: { FormatOptions: {} },
+		});
 	});
-}
+	const dir = resolve(__dirname, '../../src/test/formatting');
+	const files = readdirSync(dir);
+	const inSuffix = '.in.ahk';
+	const outSuffix = '.out.ahk';
+	for (const file of files) {
+		if (!file.endsWith(inSuffix)) continue;
+		const filenameRoot = file.slice(0, -inSuffix.length);
+		test(filenameRoot, async function () {
+			let inDoc = await vscode.workspace.openTextDocument(resolve(dir, file));
+			if (inDoc.languageId !== 'ahk2')
+				inDoc = await vscode.languages.setTextDocumentLanguage(inDoc, 'ahk2');
+			const outDoc = await vscode.workspace.openTextDocument(
+				resolve(dir, filenameRoot + outSuffix),
+			);
+			const uri = inDoc.uri.toString();
+			const params: DocumentFormattingParams = {
+				textDocument: { uri },
+				options: { insertSpaces: false, tabSize: 4 },
+			};
+			const content = outDoc.getText().replaceAll('\r\n', '\n');
+			const result: TextEdit[] | undefined = await client.sendRequest(
+				DocumentFormattingRequest.method,
+				params,
+			);
+			assert.ok(result?.[0].newText === content);
+		});
+	}
+});
