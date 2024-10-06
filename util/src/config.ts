f93476fad2d587a7c5046f8d8cce7b740faeefe4
Mark Wiemer <markwiemer@outlook.com>
2024-10-05 18:17:26 -0700

    Remove unused GlobalStorage param

--- util/src/config.ts
+++ util/src/config.ts
@@ -100,7 +100,6 @@ export interface AHKLSConfig {
 	};
 	FormatOptions: FormatOptions;
 	InterpreterPath: string;
-	GlobalStorage?: string;
 	Syntaxes?: string;
 	SymbolFoldingFromOpenBrace: boolean;
 	Warn: {
