3732cd02b3ea42c516ab032a51fb7bf9db5ffdc1
Mark Wiemer <markwiemer@outlook.com>
2024-10-06 14:42:04 -0700

    Cleanup env.ts, add some more extension commands

--- util/src/env.ts
+++ util/src/env.ts
@@ -1,39 +1,65 @@
 //* General global variables that may be changed by forks
 // For config-specific values, see `config.ts`
+
 /** ID of the language client, used for internal reference */
 export const languageClientId = 'AutoHotkey2';
+
 /** Name of the language client, displayed to users? */
 export const languageClientName = 'AutoHotkey2';
+
 /** Name of the output channel for debugging information, displayed to users */
 export const outputChannelName = 'AutoHotkey2';
 
+//* Internal commands
+
 /** Prefix for commands handled by the client. These are not registered in package.json */
 const clientCommandPrefix = 'ahk2.';
+
 export type ClientCommand = `${typeof clientCommandPrefix}${string}`;
+
 /**
  * Ask the server to execute the provided command.
  * See `executeCommandProvider` in `server/src/commandProvider.ts` for details.
  */
 export const clientExecuteCommand = `${clientCommandPrefix}executeCommand`;
+
 /** Get the active text editor URI and position */
 export const clientGetActiveEditorInfo: ClientCommand = `${clientCommandPrefix}getActiveTextEditorUriAndPosition`;
+
 /** Insert a snippet */
 export const clientInsertSnippet: ClientCommand = `${clientCommandPrefix}insertSnippet`;
+
 /** Set the language of the text document */
 export const clientSetTextDocumentLanguage: ClientCommand = `${clientCommandPrefix}setTextDocumentLanguage`;
+
 /** Update the status bar with the new interpreter info */
 export const clientUpdateStatusBar: ClientCommand = `${clientCommandPrefix}updateStatusBar`;
+
 /** Get matching AHK files */
 export const clientGetWorkspaceFiles: ClientCommand = `${clientCommandPrefix}getWorkspaceFiles`;
+
 /** Get the text content of the provided file */
 export const clientGetWorkspaceFileContent: ClientCommand = `${clientCommandPrefix}getWorkspaceFileContent`;
 
 //* ExtensionCommand values are contributed by package.json
 const extCommandPrefix = 'ahk2.';
+
 export type ExtensionCommand = `${typeof extCommandPrefix}${string}`;
+
 /** Diagnose all files */
-export const extDiagnoseAll: ExtensionCommand = `${extCommandPrefix}diagnose.all`;
+export const extDiagnoseAll = `${extCommandPrefix}diagnose.all`;
+
 /** Generate a method header comment */
-export const extGenerateComment: ExtensionCommand = `${extCommandPrefix}generate.comment`;
+export const extGenerateComment = `${extCommandPrefix}generate.comment`;
+
 /** Set the script directory */
-export const extSetScriptDir: ExtensionCommand = `${extCommandPrefix}set.scriptdir`;
+export const extSetScriptDir = `${extCommandPrefix}set.scriptdir`;
+
+/** Open AHK help */
+export const extHelp = `${extCommandPrefix}help`;
+
+/** Compile the script in the active text editor */
+export const extCompile = `${extCommandPrefix}compile`;
+
+/** Run the script in the active text editor */
+export const extRun = `${extCommandPrefix}run`;
