22d94d87c46270b6729f18ef3350a7605c700842
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2024-11-02 06:50:06 -0700

    Remove unused function (#76)

--- client/src/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from 'assert';
-import { suite, test } from 'mocha';
-import { resolvePath } from './utils';
-
-suite('resolvePath', () => {
-	test('empty string', () => {
-		assert.strictEqual(resolvePath(''), '');
-	});
-});
